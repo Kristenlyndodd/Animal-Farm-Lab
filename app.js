0860b026a8de34203ca830baa33ccf121d6cdbfa
kristenlynodd <kristenlyndodd@gmail.com>
2021-06-24 13:29:37 -0700

    Animal-farm-lab

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-// import functions
 
-// reference needed DOM elements
+//const playCat = document.getElementById('play-cat');
+const cat = document.getElementById('cat');
+const catSound = document.getElementById('cat-sound');
+const dog = document.getElementById('dog');
+const dogSound = document.getElementById('dog-sound');
+const horse = document.getElementById('horse');
+const horseSound = document.getElementById('horse-sound');
 
+cat.addEventListener('click', ()=>{
+    catSound.play();
+});
 
-// set event listeners 
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
+dog.addEventListener('click', ()=>{
+    dogSound.play();
+});
+
+horse.addEventListener('click', ()=>{
+    horseSound.play();
+});
